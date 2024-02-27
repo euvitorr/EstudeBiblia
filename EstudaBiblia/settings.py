7534a6b9e7b7c21e848ca21f380e0d14f4856229
euvitorr <vitor.r.rodrigues@hotmail.com>
2024-02-27 11:16:28 -0300

    Correção

--- EstudaBiblia/settings.py
+++ EstudaBiblia/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
         'PASSWORD': 'euvitorr',
         'HOST': 'db',  # Nome do serviço no docker-compose
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
