400959572c99a9ce130faf3d765e20862e25a0b4
euvitorr <vitor.r.rodrigues@hotmail.com>
2024-02-29 19:40:57 -0300

    Alterações de acesso para digital ocean

--- EstudaBiblia/settings.py
+++ EstudaBiblia/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bible',
-        'USER': 'euvitorr',
-        'PASSWORD': 'euvitorr',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': 'db',  # Nome do serviço no docker-compose
         'PORT': '3306',
         'OPTIONS': {
